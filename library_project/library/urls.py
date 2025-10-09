efe02b703a337e02e670e09302b0720d51a6c155
Benedict Nderitu <giteebenedict@gmail.com>
2025-10-09 10:08:10 +0300

    Add files via upload

---
+++ library_project/library/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, UserViewSet, CheckoutAPIView, ReturnAPIView, UserTransactionsListView
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
+router.register(r'users', UserViewSet, basename='user')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('books/<int:book_id>/checkout/', CheckoutAPIView.as_view(), name='book-checkout'),
+    path('books/<int:book_id>/return/', ReturnAPIView.as_view(), name='book-return'),
+    path('transactions/', UserTransactionsListView.as_view(), name='user-transactions'),
+]
